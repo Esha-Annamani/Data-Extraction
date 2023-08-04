fe06d7cd6e3a483482946a77b7c9a56c14f0cedc
Ashish-Bounteous <141404711+Ashish-Bounteous@users.noreply.github.com>
2023-08-04 14:53:57 +0530

    Delete Main.js

--- Main.js
+++
@@ -1,4 +0,0 @@
-const { readAndExtractData } = require("./ReadAndExtractData");
-
-const fileNames = ["./files/textfile.txt", "./files/jsondata.json"]
-fileNames.forEach(fileName => readAndExtractData(fileName))
